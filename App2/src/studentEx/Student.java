3d55a70efe944d13c900133dc23e44390d559a46
SiddheshwarKanhere <siddheshwarkanhere79@gmail.com>
2024-05-28 17:03:54 +0530

    Unit Test folder

---
+++ App2/src/studentEx/Student.java
@@ -0,0 +1,16 @@
+package studentEx;
+
+import userEx.User;
+
+public class Student {
+	public static void main(String[] args) {
+		
+		User user = new User(10,"Kanhere");
+		user.speak();
+		
+		
+		
+		
+	}
+
+}
